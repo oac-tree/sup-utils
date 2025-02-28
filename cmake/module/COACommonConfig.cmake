83482f6e42ff5ba4ed43e992e04f0364eeccefdb
Walter Van Herck <walter.vanherck@iter.org>
2025-02-28 09:30:16 +0000

    Extract common cmake configuration

---
+++ cmake/module/COACommonConfig.cmake
@@ -0,0 +1,33 @@
+# -----------------------------------------------------------------------------
+# Main project configuration
+# -----------------------------------------------------------------------------
+
+include(GNUInstallDirs)
+include(CTest)
+include(COASetupCodacEnvironment)
+
+# -----------------------------------------------------------------------------
+# C++ version
+# -----------------------------------------------------------------------------
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+if(NOT CMAKE_BUILD_TYPE)
+  set(CMAKE_BUILD_TYPE "RelWithDebInfo")
+endif()
+
+# -----------------------------------------------------------------------------
+# Variables
+# -----------------------------------------------------------------------------
+set(LIBVERSION ${PROJECT_VERSION})
+set(LIBSOVERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR})
+
+# -----------------------------------------------------------------------------
+# Directories
+# -----------------------------------------------------------------------------
+if(NOT DEFINED TEST_OUTPUT_DIRECTORY)
+  set(TEST_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/test_bin)
+endif()
+
+file(MAKE_DIRECTORY ${TEST_OUTPUT_DIRECTORY})
--- cmake/module/configuration.cmake
+++ cmake/module/configuration.cmake
@@ -1,35 +1,8 @@
 # -----------------------------------------------------------------------------
-# Modules
+# Common project configuration
 # -----------------------------------------------------------------------------
-include(GNUInstallDirs)
-include(CTest)
-include(COASetupCodacEnvironment)
 
-# -----------------------------------------------------------------------------
-# C++ version
-# -----------------------------------------------------------------------------
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
-if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "RelWithDebInfo")
-endif()
-
-# -----------------------------------------------------------------------------
-# Variables
-# -----------------------------------------------------------------------------
-set(LIBVERSION ${CMAKE_PROJECT_VERSION})
-set(LIBSOVERSION ${CMAKE_PROJECT_VERSION_MAJOR}.${CMAKE_PROJECT_VERSION_MINOR})
-
-# -----------------------------------------------------------------------------
-# Directories
-# -----------------------------------------------------------------------------
-if(NOT DEFINED TEST_OUTPUT_DIRECTORY)
-  set(TEST_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/test_bin)
-endif()
-
-file(MAKE_DIRECTORY ${TEST_OUTPUT_DIRECTORY})
+include(COACommonConfig)
 
 # -----------------------------------------------------------------------------
 # Dependencies
