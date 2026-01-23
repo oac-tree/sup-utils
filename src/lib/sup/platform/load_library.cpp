20272856f85282d6b2e30cb10672c976b5f196ab
Walter Van Herck <walter.vanherck@iter.org>
2026-01-23 12:38:00 +0000

    Add dynamic loading of libraries to platform library

---
+++ src/lib/sup/platform/load_library.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation System Utilities
+ *
+ * Description   : SUP platform dependent utilities
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2026 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/platform/load_library.h>
+
+#if defined(_WIN32)
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
+
+namespace sup
+{
+namespace platform
+{
+
+void* LoadLibrary(const std::string& library_path)
+{
+#if defined(_WIN32)
+  HMODULE handle = ::LoadLibraryA(path.c_str());
+  return reinterpret_cast<void*>(handle);
+#else
+  void* handle = ::dlopen(library_path.c_str(), RTLD_NOW);
+  return handle;
+#endif
+}
+
+void UnloadLibrary(void* handle)
+{
+  if (!handle)
+  {
+    return;
+  }
+#if defined(_WIN32)
+  ::FreeLibrary(reinterpret_cast<HMODULE>(handle));
+#else
+  (void)::dlclose(handle);
+#endif
+}
+
+}  // namespace platform
+
+}  // namespace sup
---
+++ src/lib/sup/platform/load_library.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation System Utilities
+ *
+ * Description   : SUP platform dependent utilities
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2026 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_PLATFORM_LOAD_LIBRARY_H_
+#define SUP_PLATFORM_LOAD_LIBRARY_H_
+
+#include <string>
+
+namespace sup
+{
+namespace platform
+{
+
+/**
+ * @brief Try to dynamically load the given library.
+ *
+ * @param library_path Path of the library (may be relative or absolute). Names without `/` will
+ * also be looked up in the system's library paths.
+ * @return Handle to the library or nullptr when loading failed.
+ */
+void* LoadLibrary(const std::string& library_path);
+
+/**
+ * @brief Try to unload a dynamically loaded library.
+ *
+ * @param handle Handle to loaded library.
+ */
+void UnloadLibrary(void* handle);
+
+}  // namespace platform
+
+}  // namespace sup
+
+#endif  // SUP_PLATFORM_LOAD_LIBRARY_H_
